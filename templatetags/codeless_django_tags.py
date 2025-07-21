eace147c2783e4b5cbf611bf90ee9084305fa8a6
Omar Faruk <84440130+omariut@users.noreply.github.com>
2025-07-21 06:39:34 +0600

    Add utility for OS file path resolution and update DataManager
    Introduced get_os_file_path in utils.py to resolve file paths relative to the codeless_django package. Updated DataManager initialization in codeless_django_tags.py to use this utility for loading fields.json.

--- templatetags/codeless_django_tags.py
+++ templatetags/codeless_django_tags.py
@@ -1,8 +1,10 @@
 from django import template
+from codeless_django.utils import get_os_file_path
 from codeless_django.data_manager import DataManager
 
 register = template.Library()
-data_manager=DataManager(file='codeless_django/fields.json')
+
+data_manager=DataManager(file=get_os_file_path('fields.json'))
 
 @register.filter
 def get_options_with_default_values(value):
