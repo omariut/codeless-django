ea6cfa2e2e0717ce6b90f7acf565b3a683d5f5e8
Omar Faruk <84440130+omariut@users.noreply.github.com>
2025-07-21 07:50:37 +0600

    Update documentation.py

--- writers/documentation.py
+++ writers/documentation.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 import os
-
+from codeless_django.utils import get_os_file_path
 class DocumentationWriter:
     def __init__(self):
-        self.file_name=settings_file=settings.ROOT_URLCONF.split(".")[0] + "/urls.py"
+        self.file_name=settings.ROOT_URLCONF.split(".")[0] + "/urls.py"
 
     def write_documentation_url(self):
-        os.system(f"cp codeless_django/additional_files/root_urls.py {self.file_name}")
+        os.system(f"cp {get_os_file_path('additional_files/root_urls.py')} {self.file_name}")
